71871840afe7d356e6bb341a50822768cbe22f14
Suman Kisku <sumankisku1@gmail.com>
2023-01-25 00:39:48 +0530

    push

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+const h3 = document.getElementById("wordCount");
+
+textbox.addEventListener("change", ()=> {
+    countWords();
+});
+
+function countWords() {
+    let words = textbox.value.split(" ").filter((str)=> str !== '');
+    h3.innerText = `${words.length}`;
+    document.body.appendChild(h3);
+    document.body.appendChild(textbox);
+
+}
+
+countWords();
